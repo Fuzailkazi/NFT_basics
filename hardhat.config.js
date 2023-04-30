ab5aaec30b38e2e7ab51c9e7d7c68d8c9a06ce3e
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2023-04-30 15:59:13 +0530

    ERC 721 contract done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_URL,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+  },
 };
